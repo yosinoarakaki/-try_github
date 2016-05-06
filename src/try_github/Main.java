c7225098f6cad106be4dc2fcc75109bd311a676f
iTAP <iTAP@iTAP-PC13>
2016-05-06 16:41:23 +0900

    標準出力へ文字を表示

--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -3,6 +3,7 @@ package try_github;
 public class Main {
 
     public static void main(String[] args) {
+    	System.out.println("Hello Git with Eclipse");
         // TODO Auto-generated method stub
 
     }
